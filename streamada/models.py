fb2193eab218114d8cf136ebfd599d3c15347a5f
Hamzaba92 <hamzabaj1992@gmail.com>
2024-10-02 05:45:25 +0200

    implement a validation when uploading a videofile

--- streamada/models.py
+++ streamada/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 GENRE_CHOICES = [
     ('new_on_streamada', 'New on Streamada'),
@@ -9,11 +10,19 @@ GENRE_CHOICES = [
     ('abstract', 'Abstract'),
 ]
 
+def validate_video_file(value):
+    """Validate the Uploadfile if its truly a videofile"""
+
+    if not value.name.endswith(('.mp4', '.mov', '.avi', '.mkv')):
+        raise ValidationError("Only Videofiles which ends with '.mp4', '.mov', '.avi', '.mkv'")
+
+
+
 class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
-    video_file = models.FileField(upload_to='videos/')
+    video_file = models.FileField(upload_to='videos/', validators=[validate_video_file])
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
